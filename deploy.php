3fc3b6acb2e3fdd72749d37928fec03e37ba0d3c
haley nguyen <hhn007@ucsd.edu>
2026-01-24 09:07:28 +0000

    Add deploy.php for webhooks

---
+++ deploy.php
@@ -0,0 +1,44 @@
+<?php
+    chdir('/home/haley/deploy/cse135-site/home');
+    
+    // array of commands
+    $commands = array(
+        'echo $PWD',
+        'whoami',
+        'git pull origin main',
+        'git status',
+    );
+
+    // exec commands
+    $output = '';
+    foreach($commands AS $command){
+        $tmp = shell_exec($command);
+        
+        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+    }
+    
+    // Run deployment script
+    $deploy_output = shell_exec('sudo /usr/local/bin/deploy-cse135.sh 2>&1');
+    $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">sudo /usr/local/bin/deploy-cse135.sh\n</span><br />";
+    $output .= htmlentities(trim($deploy_output)) . "\n<br /><br />";
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
+
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
