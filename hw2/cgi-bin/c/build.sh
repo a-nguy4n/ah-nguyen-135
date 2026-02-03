5bc9ffe5b0c92d855d8740e83ec99f9234c1a017
a-nguy4n <1512alli@gmail.com>
2026-02-02 20:37:15 -0800

    Path Fix Rename

--- hw2/cgi-bin/c/build.sh
+++ hw2/cgi-bin/c/build.sh
@@ -5,7 +5,7 @@ gcc hello-json-c.c -o hello-json-c.cgi
 gcc environment-c.c -o environment-c.cgi
 gcc echo-c.c -o echo-c.cgi
 
-gcc -O2 state_demo-c.c -o state_demo-c.cgi
+gcc -O2 state-demo-c.c -o state-demo-c.cgi
 
 chmod 755 *.cgi
 
