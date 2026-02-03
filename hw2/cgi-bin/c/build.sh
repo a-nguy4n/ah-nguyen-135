4212c7294fe2268d068d80a82e428431aea0df5a
a-nguy4n <1512alli@gmail.com>
2026-02-02 20:25:33 -0800

    C State Form and build updated

--- hw2/cgi-bin/c/build.sh
+++ hw2/cgi-bin/c/build.sh
@@ -5,6 +5,8 @@ gcc hello-json-c.c -o hello-json-c.cgi
 gcc environment-c.c -o environment-c.cgi
 gcc echo-c.c -o echo-c.cgi
 
+gcc -O2 state_demo-c.c -o state_demo-c.cgi
+
 chmod 755 *.cgi
 
 echo "Build complete."
