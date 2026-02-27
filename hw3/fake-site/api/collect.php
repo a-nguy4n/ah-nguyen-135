3755d3e8655bfb10345a803cb4c3d451e8304dcb
nguyenhaley <130112171+nguyenhaley@users.noreply.github.com>
2026-02-27 13:47:01 -0800

    completed collect.php

--- hw3/fake-site/api/collect.php
+++ hw3/fake-site/api/collect.php
@@ -16,7 +16,6 @@ if ($conn->connect_error) {
 
 // inserts static data
 $static = $data['static'];
-
 $stmt = $conn->prepare("INSERT INTO static_data 
     (session_id, user_agent, language, cookies_enabled, viewport_width, viewport_height, screen_width, screen_height, pixel_ratio, cores, memory, network_type, network_downlink, network_rtt, color_scheme, timezone, javascript_enabled, images_enabled, css_enabled) 
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
@@ -42,6 +41,44 @@ $stmt->bind_param("sssiiiiiddsddiissii",
     $data['static']['javascriptEnabled'],
     $data['static']['imagesEnabled'],
     $data['static']['cssExternalLoaded']
+);
+$stmt->execute();
+
+// inserts performance data
+$perf = $data['performance'];
+$stmt = $conn->prepare("INSERT INTO performance_data 
+    (session_id, load_start, load_end, total_load_time, raw_timing) 
+    VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sddds",
+    $data['sessionId'],
+    $perf['loadStart'],
+    $perf['loadEnd'],
+    $perf['totalLoadTime'],
+    json_encode($perf['rawTiming'])
+);
+$stmt->execute();
+
+// inserts activity data
+$activity = $data['activity']['activityState'];
+$stmt = $conn->prepare("INSERT INTO activity_data
+    (session_id, entered_at, left_at, time_on_page_ms, mouse_moves, last_cursor, clicks, scroll, key_presses, key_releases, errors, error_count, idle_breaks, total_idle_time_ms, page_url) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("siiiissssssisis",
+    $data['sessionId'],
+    $activity['enteredAt'],
+    $activity['leftAt'],
+    $activity['timeOnPageMs'],
+    $activity['mouseMoves'],
+    json_encode($activity['lastCursor']),
+    json_encode($activity['clicks']),
+    json_encode($activity['scroll']),
+    json_encode($activity['keyPresses']),
+    json_encode($activity['keyReleases']),
+    json_encode($activity['errors']),
+    $activity['errorCount'],
+    json_encode($activity['idle']['breaks']),
+    $activity['idle']['totalIdleTimeMs'],
+    $activity['pageUrl']
 );
 $stmt->execute();
 
