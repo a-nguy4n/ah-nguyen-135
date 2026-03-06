d2169786ca564b3250848b2bec642882e60a0ca3
a-nguy4n <1512alli@gmail.com>
2026-03-05 19:55:24 -0800

    comment deletion

--- hw3/reporting/app/views/dashTwo.php
+++ hw3/reporting/app/views/dashTwo.php
@@ -1,15 +1,3 @@
-<!-- 
-<!DOCTYPE html>
-<html>
-<head><title>Dashboard</title></head>
-<body>
-    <h1>Analytics Dashboard</h1>
-    <p>Welcome, <?= $_SESSION['user'] ?></p>
-    <a href="/logout">Logout</a>
-</body>
-</html>
--->
-
 <!DOCTYPE html>
 <html>
 <head><title>Dashboard</title></head>
