34d0f57fbbfe6968fd6e73e1a91bc05ee924a54b
nguyenhaley <130112171+nguyenhaley@users.noreply.github.com>
2026-03-12 22:18:33 -0700

    changed landing page

--- hw3/reporting/app/controllers/dashboardController.php
+++ hw3/reporting/app/controllers/dashboardController.php
@@ -3,19 +3,5 @@ if (empty($_SESSION['user'])) {
     header('Location: /login');
     exit;
 }
-
-require APP . '/models/staticData.php';
-$model = new staticData();
-$staticData = $model->getAll();
-
-require APP . '/models/performanceData.php';
-$model = new performanceData();
-$performanceData = $model->getAll();
-
-require APP . '/models/activityData.php';
-$model = new activityData();
-$activityData = $model->getAll();
-
-# require APP . '/views/dashboard.php';
 require ROOT . '/final-project/dashboard.html';
 ?>
