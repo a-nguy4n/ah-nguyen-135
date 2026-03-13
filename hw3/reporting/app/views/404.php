b61bb63bdc8f7a24371b4b96173b456795e995da
nguyenhaley <130112171+nguyenhaley@users.noreply.github.com>
2026-03-13 14:29:36 -0700

    fixed links

--- hw3/reporting/app/views/404.php
+++ hw3/reporting/app/views/404.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 - Page Not Found</title>
-    <style>  body {color: red;}  </style>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="/project/dashboard.css">
 </head>
 <body>
     <h1><b>404<b></h1>
