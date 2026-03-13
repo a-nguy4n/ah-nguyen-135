15f503a9d94e2475f96fc61492ae96e527fc4e2b
nguyenhaley <130112171+nguyenhaley@users.noreply.github.com>
2026-03-12 22:52:46 -0700

    changed directory structure

--- hw3/reporting/app/controllers/dashboardController.php
+++ hw3/reporting/app/controllers/dashboardController.php
@@ -3,5 +3,5 @@ if (empty($_SESSION['user'])) {
     header('Location: /login');
     exit;
 }
-require '/var/www/ah-nguyen.site/public_html/final-project/dashboard.html';
+require ROOT . '/project/dashboard.html';
 ?>
